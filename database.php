5516b0cc50c0c92b48e35aff90a0b2baca2e2ebe
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-05-24 16:30:51 +0200

    aggiornato css

--- database.php
+++ database.php
@@ -2,10 +2,10 @@
 $faq = [
   [
     'question' => "<h2>Come state implementando la recente decisione della Corte di giustizia dell&lsquo;Unione europea &#40;CGUE&#41; relativa al diritto all&lsquo;oblio?</h2>",
-    'answer' => "<p>La recente decisione della Corte di giustizia dell&lsquo;Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.</p>
+    'answer' => "<p>La recente <a href='http://curia.europa.eu/juris/document/document.jsf?text=&docid=152065&amppageIndex=0&doclang=it&mode=lst&dir=&occ=first&part=1&cid=276332'>decisione della Corte di giustizia dell&lsquo;Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.</p>
 
     <p>Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell&lsquo;individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.</p>
-    <p>Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po, di tempo perché abbiamo già ricevuto tante richieste simili), Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio &#40;eletto o non eletto&#41;. Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all&lsquo;Autorità garante per la protezione dei dati personali nel tuo paese.</p>
+    <p>Per presentare una richiesta di rimozione, compila questo <a href='https://support.google.com/legal/contact/lr_eudpa?product=websearch&hl=it'>modulo web</a>. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po, di tempo perché abbiamo già ricevuto tante richieste simili), Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio &#40;eletto o non eletto&#41;. Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all&lsquo;Autorità garante per la protezione dei dati personali nel tuo paese.</p>
     
     <p>Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.</p>
     
@@ -18,26 +18,30 @@ $faq = [
 
     <p>Siamo costantemente al lavoro per garantire un&lsquo;elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.</p>
     
-    <p>Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.</p>
+    <p>Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a hfre='https://www.google.com/safetycenter/'>Centro Google per la sicurezza online</a>.</p>
     
-    <p>Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.</p>"
+    <p><a href='https://privacy.google.com/'>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.</p>"
   ],
   [
     'question' => "<h2>Perché il mio account è associato a un paese?</h2>",
     'answer' => "<p>Il tuo account è associato a un paese &#40;o territorio&#41; nei Termini di servizio per poter stabilire due cose:</p>
-
-    <p>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:</p>
-    <p>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo &#40;paesi dell&lsquo;Unione europea, oltre a Islanda, Liechtenstein e Norvegia&#41; o in Svizzera.</p>
-    <p>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</p>
-    <p>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</p>
+      <ol>
+        <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:</li>
+          <ol class='order'>
+            <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo &#40;paesi dell&lsquo;Unione europea, oltre a Islanda, Liechtenstein e Norvegia&#41; o in Svizzera.</li>
+            <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
+          </ol>
+        <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
+      </ol>
+    
     <p>Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.</p>
     
-    <p>Stabilire il paese associato al tuo account</p>
+    <h5>Stabilire il paese associato al tuo account</h5>
     <p>Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell&lsquo;ultimo anno.</p>
     
     <p>I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l&lsquo;associazione del paese.</p>
     
-    <p>Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l&lsquo;installazione di una rete privata virtuale &#40;VPN&#41; per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato.</p>"
+    <p>Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l&lsquo;installazione di una rete privata virtuale &#40;VPN&#41; per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. <a href='https://policies.google.com/country-association-contact-us'>Contattaci<a/> se ritieni che il paese associato al tuo account sia sbagliato.</p>"
     ],
   [
     'question' => "<h2>Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?</h2>",
--- index.php
+++ index.php
@@ -19,21 +19,24 @@ include 'database.php';
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- Bootstrap -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+  <!-- CSS Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <!-- Font Google -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
   <!-- Style -->
   <link rel="stylesheet" href="css/style.css">
   <title>Php Google Faq</title>
 </head>
 <body>
   <main>
-    <div> 
+    <div class="container"> 
     <ul>
         <?php 
         for( $i= 0; $i < count($faq); $i++){ ?>
         <li>
-        <?php echo "{$faq[$i]['question']} - {$faq[$i]['answer']} <br>"; ?>
+        <?php echo "{$faq[$i]['question']} {$faq[$i]['answer']} <br>"; ?>
         </li>
         <?php 
         }
